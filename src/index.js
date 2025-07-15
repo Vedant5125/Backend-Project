381ae287e77331281ae32cfb5d3ed6da075775cb
Vedant5125 <vedantapraj5125@gmail.com>
2025-07-16 01:32:40 +0530

    route

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 // require("dotenv").config({path: "./.env" });
-
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 
 
-
 connectDB()
 .then(()=>{
-    app.on((err)=>{
+    app.on("error", (err)=>{
         console.error("Error in Express app:", err);
     })
     app.listen(process.env.PORT || 8000, ()=>{
